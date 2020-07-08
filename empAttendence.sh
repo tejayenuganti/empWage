7d16d2c9ef21b41cf4641e02331ff9fadd72ec79
Teja <tejasriyenuganti@gmail.com>
2020-07-08 23:29:14 +0530

    Employee wage per month

--- empAttendence.sh
+++ empAttendence.sh
@@ -2,12 +2,16 @@
 
 fullDayHour=8
 wagePerHour=20
+daysInMonth=20
 empPartTime=8
 attendence=$((RANDOM%2));
 echo $attendence
 
 case $attendence in
 	1)
+	monthWage=$(($daysInMonth * $wagePerHour))
+	echo "wages for a month is :" $monthWage
+
 	partTimeWage=$(( $empPartTime * $wagePerHour ))
 	dayWage=$(( $fullDayHour * $wagePerHour ))
 		echo "Employee wage per day is " $dayWage
