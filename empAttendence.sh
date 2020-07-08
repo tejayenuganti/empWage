a531ac678dac2b282fea371c510af59d14ea7841
Teja <tejasriyenuganti@gmail.com>
2020-07-08 21:54:06 +0530

    daily wage of employee

--- empAttendence.sh
+++ empAttendence.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -x
 
+wagePerHour=20
+fullDayHour=8
+
 attendence=$((RANDOM%2));
 echo $attendence
 
 if [[ $attendence -eq 0 ]]
 then
 		echo "Employee is present"
+		empWage=$(( $wagePerHour*$fullDayHour ))
+		echo "Daily wage of employee" $empWage
+
 else
 		echo "Employee is absent"
 fi
