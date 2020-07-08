194d2e781f0462a6ce7bcf1c3275c9874d246951
Teja <tejasriyenuganti@gmail.com>
2020-07-08 23:03:10 +0530

    add parttime and wage

--- empAttendence.sh
+++ empAttendence.sh
@@ -2,14 +2,17 @@
 
 fullDayHour=8
 wagePerHour=20
-
+empPartTime=8
 attendence=$((RANDOM%2));
 echo $attendence
 
 if [[ $attendence -eq 0 ]]
 then
-	dayWage=$(($fullDayHour * $wagePerHour ))
+	partTimeWage=$(( $empPartTime * $wagePerHour ))
+	dayWage=$(( $fullDayHour * $wagePerHour ))
 		echo "Employee wage per day is " $dayWage
+
+		echo "Partime wage is" $partTimeWage
 else
 		echo "Employee is absent"
 fi
